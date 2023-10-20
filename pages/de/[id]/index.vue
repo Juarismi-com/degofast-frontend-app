6abb2c9d8b211d0b0bda54015b64ba537075818b
Ivan <vanfretes@gmail.com>
2023-10-19 22:20:24 -0300

    feat: title for de page

--- pages/de/[id]/index.vue
+++ pages/de/[id]/index.vue
@@ -1,7 +1,7 @@
 <template>
    <div>
       <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-         Documento Electronicos
+         {{ title }}
       </h2>
       <DEList />
    </div>
@@ -10,13 +10,22 @@
 <script setup>
 import { TIPO_DOCUMENT_LIST } from "../../../config";
 
+const route = useRoute();
+
 definePageMeta({
    middleware: ["auth"],
 });
 
-const titles = ref(TIPO_DOCUMENT_LIST);
+const routeList = ref(TIPO_DOCUMENT_LIST);
 
-onMounted(() => {
-   console.log("onMounted");
-});
+const routeSelected = routeList.value.find(
+   (item) => item.tipoDocumento == route.params.id,
+);
+
+const title = ref(routeSelected.title);
+
+/*const setTitle = () => {
+   const routeSelected = console.log(route.params.id);
+   title.value = routeSelected.title;
+};*/
 </script>
