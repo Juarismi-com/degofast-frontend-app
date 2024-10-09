12f67a34d572a5c332d881a4da4e5502034b208d
ATellez22 <adolfo.tellez1991@gmail.com>
2024-10-09 17:18:56 -0300

    ordered-recibo-list

--- components/Receipt/ReceiptList.vue
+++ components/Receipt/ReceiptList.vue
@@ -36,9 +36,8 @@
             <thead>
                <tr
                   class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
-                  <th class="px-4 py-3">Numero</th>
                   <th class="px-4 py-3">Recibido de</th>
-                  <th class="px-4 py-3">Concepto</th>
+                  <th class="px-4 py-3">Numero</th>
                   <th class="px-4 py-3">Fecha</th>
                   <th class="px-4 py-3">Monto</th>
                   <th class="px-4 py-3">Estado</th>
@@ -47,21 +46,8 @@
             </thead>
             <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
                <tr class="text-gray-700 dark:text-gray-400" v-for="(item, index) in filteredItems" :key="index">
-                  <td class="px-4 py-3">
-                     <div class="flex items-center text-sm">
-                        <div class="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-                           <img class="object-cover w-full h-full rounded-full"
-                              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
-                              alt="" loading="lazy" />
-                           <div class="absolute inset-0 rounded-full shadow-inner" aria-hidden="true"></div>
-                        </div>
-                        <div>
-                           {{ item?.numero }}
-                        </div>
-                     </div>
-                  </td>
                   <td class="px-4 py-3 text-sm">{{ item.recibidoDe }}</td>
-                  <td class="px-4 py-3 text-sm">{{ item.concepto }}</td>
+                  <td class="px-4 py-3 text-sm">{{ item.numero }}</td>
                   <td class="px-4 py-3 text-sm">
                      {{ moment(item.fecha).format("DD/MM/YYYY") }}
                   </td>
