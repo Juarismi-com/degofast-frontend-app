8cf0d572e99c22601b1ab91a60d4f0f54181e100
ATellez22 <adolfo.tellez1991@gmail.com>
2024-10-09 11:44:53 -0300

    added-recibo-view

--- components/Receipt/ReceiptList.vue
+++ components/Receipt/ReceiptList.vue
@@ -5,7 +5,7 @@
       </h2>
 
       <div class="col-span-3 flex justify-left">
-         <a :href="`/receipt/receiptForm`"
+         <a :href="`/receipt/receipt-form`"
             class="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
             type="submit">
             Nuevo
