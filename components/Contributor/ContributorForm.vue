147320dab831ad4ce8862c9bd0296535e25ebd25
ATellez22 <adolfo.tellez1991@gmail.com>
2024-10-11 17:52:46 -0300

    adjust-steps-in-contributor

--- components/Contributor/ContributorForm.vue
+++ components/Contributor/ContributorForm.vue
@@ -4,7 +4,8 @@
          Contribuyente
       </h2>
 
-      <DEStepperFull :current-step="currentStep" :steps="steps" :next-step-fn="setCurrentStep" :btn-control="true">
+      <DEStepperFull :current-step="currentStep" :steps="steps" :next-step-fn="setCurrentStep" :btn-control="true"
+         :step-availability="availableSteps">
          <div>
             <div v-if="currentStep == 0" class="max-w p-6 bg-white border border-gray-200 rounded-lg shadow">
                <ContributorFormFullContributorDataComponent :contributor="contributor" />
@@ -45,14 +46,18 @@
  */
 import { storeToRefs } from "pinia";
 import { useAuthStore } from "~/stores";
+import { getPuntoExpedicionByFilters } from "~/services/punto-expedicion.service.ts";
 
 // datos del contribuyente
 const authStore = useAuthStore();
 const { contributor } = storeToRefs(authStore);
+const puntoExpedicionList = ref([])
 
 // current form view
 const currentStep = ref(0);
 
+
+// manejo de pestañas
 const steps = [
    {
       title: "Datos del contribuyente",
@@ -76,10 +81,28 @@ const steps = [
 ];
 
 const setCurrentStep = (value) => {
-   currentStep.value = value;
+   if (availableSteps.value[value]) {
+      currentStep.value = value;
+   }
 };
 
-// const registerFail = ref(false);
+const availableSteps = computed(() => {
+   return [
+      true,
+      contributor.value?._id ? true : false,
+      contributor.value?.establecimientos ? true : false,
+      puntoExpedicionList.value.length > 0 ? true : false,
+      contributor.value?.certData?.length > 0 ? true : false
+   ];
+});
+
+onMounted(async () => {
+   puntoExpedicionList.value = await getPuntoExpedicionByFilters({
+      contributor: contributor.value?._id
+   });
+});
+
+
 
 // const setContributorDefault = () => {
 //    contributor;
