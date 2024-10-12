1fa60dbc5990a69accca03433fc130385ecaeba8
ATellez22 <adolfo.tellez1991@gmail.com>
2024-10-12 18:26:31 -0300

    added-correct-validators

--- config/receipt.ts
+++ config/receipt.ts
@@ -14,11 +14,20 @@ export const deReceiptData = {
 export const validateRecibo = (recibo: any) => {
    try {
 
-      if (!recibo.numero) throw "recibo.numero es requerido";
-      if (!recibo.recibidoDe) throw "recibo.recibidoDe es requerido";
+      console.log(typeof recibo.montoLetras);
+
+      if (!recibo.numero) throw "El campo numero es requerido";
+      if (!recibo.recibidoDe) throw "El campo recibidoDe es requerido";
       if (!recibo.monto || isNaN(Number(recibo.monto))) throw "El campo 'monto' es requerido y debe ser numérico";
-      if (!recibo.montoLetras) throw "recibo.montoLetras es requerido";
-      if (!recibo.concepto) throw "recibo.concepto es requerido";
+      if (!recibo.moneda) throw "Moneda es requerido";
+
+      if (!recibo.montoLetras) throw "El campo montoLetras es requerido";
+      if (typeof recibo.montoLetras === 'number') throw "El campo 'montoLetras' no debe ser numérico";
+      if (/\d/.test(recibo.montoLetras)) {
+         throw "El campo montoLetras no debe contener números.";
+      }
+
+      if (!recibo.concepto) throw "El campo concepto es requerido";
 
       return true;
    } catch (error) {
