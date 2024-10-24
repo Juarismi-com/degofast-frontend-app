75cd72d949c9da2f6b754fb897a187e9df5e58e7
ATellez22 <adolfo.tellez1991@gmail.com>
2024-10-24 14:10:50 -0300

    feat/adjust_of_alert

--- components/DE/Message/EnvioComponent.vue
+++ components/DE/Message/EnvioComponent.vue
@@ -43,13 +43,13 @@
          </li>
       </ul>
    </div>
-   <div class="mt-4" v-if="submitDeSuccess">
+   <div class="mt-4">
       <div
          class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
          role="alert"
       >
-         <span class="font-medium">Aviso: </span> Las facturas presentadas no se
-         podrán anular después de 48 horas.
+         <span class="font-medium">Aviso: </span> Los documentos electrónicos no
+         se podrán anular después de 48 horas.
       </div>
    </div>
 </template>
