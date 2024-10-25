1541c116f376ba4899adc10225063a9f7d9ef12d
ATellez22 <adolfo.tellez1991@gmail.com>
2024-10-25 15:30:07 -0300

    feat: adjust-de-coutas-table-banco-efectivo

--- config/receipt.ts
+++ config/receipt.ts
@@ -38,6 +38,17 @@ export const validateRecibo = (recibo: any) => {
       }
       if (!recibo.concepto) throw "El campo concepto es requerido";
 
+      // Validación adicional para Cheque o Transferencia
+      if (
+         recibo.formaPago === "Cheque" ||
+         recibo.formaPago === "Transferencia"
+      ) {
+         if (!recibo.banco)
+            throw "El campo 'banco' es requerido para pagos con Cheque o Transferencia";
+         if (!recibo.nroBanco)
+            throw "El campo 'N° Banco' es requerido para pagos con Cheque o Transferencia";
+      }
+
       return true;
    } catch (error) {
       throw error;
