101aae5c9bcb883fa0d6bcd8cafb32127c3ab6e0
ATellez22 <adolfo.tellez1991@gmail.com>
2025-01-15 10:27:09 -0300

    feat:preview-table-adjust

--- components/DE/Preview/FormatBasic.vue
+++ components/DE/Preview/FormatBasic.vue
@@ -3,24 +3,38 @@
       <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
          <!-- Cuadro 1 -->
 
-         <div class="rounded border border-gray-300 p-4 shadow-md md:col-span-3">
-            <h3 class="-mx-4 -mt-4 rounded-t-lg bg-gray-300 px-4 py-2 text-center text-sm font-medium text-gray-800">
+         <div
+            class="rounded border border-gray-300 p-4 shadow-md md:col-span-3"
+         >
+            <h3
+               class="-mx-4 -mt-4 rounded-t-lg bg-gray-300 px-4 py-2 text-center text-sm font-medium text-gray-800"
+            >
                KuDE de Factura Electrónica
             </h3>
             <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
                <div class="col-span-1 grid grid-cols-1 md:col-span-3">
                   <table class="w-full border-collapse">
                      <tr>
-                        <td class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-lg font-semibold">
-                           {{ detalle.tipoDocumento == 1 ? "FACTURA ELECTRÓNICA" : "NOTA DE CRÉDITO" }}: <br />
+                        <td
+                           class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-lg font-semibold"
+                        >
+                           {{
+                              detalle.tipoDocumento == 1
+                                 ? "FACTURA ELECTRÓNICA"
+                                 : "NOTA DE CRÉDITO"
+                           }}: <br />
                            {{ detalle.establecimiento }} - 001 - 00Y
                            <br />
                         </td>
-                        <td class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-base font-normal">
+                        <td
+                           class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-base font-normal"
+                        >
                            Timbrado N°:
-                           <label class="font-bold">{{
-                              authStore.contributor.timbradoNumero
-                              }} </label><br />
+                           <label class="font-bold"
+                              >{{
+                                 authStore.contributor.timbradoNumero
+                              }} </label
+                           ><br />
 
                            Fecha de timbrado:
                            <label class="font-bold">
@@ -28,7 +42,8 @@
                                  moment(
                                     authStore.contributor.timbradoFecha,
                                  ).format("DD/MM/YYYY")
-                              }}</label>
+                              }}</label
+                           >
                            <br />
                         </td>
                      </tr>
@@ -38,12 +53,16 @@
          </div>
 
          <!-- Cuadro 2 -->
-         <div class="rounded border border-gray-300 p-2 shadow-md md:col-span-3">
+         <div
+            class="rounded border border-gray-300 p-2 shadow-md md:col-span-3"
+         >
             <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
                <div class="col-span-1 grid grid-cols-1 md:col-span-3">
                   <table class="w-full border-collapse">
                      <tr>
-                        <td class="w-1/2 px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 px-2 py-1 text-left text-sm font-medium"
+                        >
                            Fecha y hora de emisión:
                            <label class="font-bold">{{
                               moment(props.detalle.fecha).format(
@@ -51,60 +70,79 @@
                               )
                            }}</label>
                         </td>
-                        <td class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium"
+                        >
                            RUC/Documento de identidad N°:
                            <label class="font-bold">{{
                               props.detalle.cliente.ruc
-                              }}</label>
+                           }}</label>
                         </td>
                      </tr>
                      <tr>
-                        <td class="w-1/2 px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 px-2 py-1 text-left text-sm font-medium"
+                        >
                            Condición de venta:
                            <label class="font-bold">{{
                               props.detalle.condicionName
-                              }}</label>
+                           }}</label>
                         </td>
-                        <td class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium"
+                        >
                            Nombre o Razón Social:
                            <label class="font-bold">{{
                               props.detalle.cliente.razonSocial
-                              }}</label>
+                           }}</label>
                         </td>
                      </tr>
                      <tr>
-                        <td class="w-1/2 px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 px-2 py-1 text-left text-sm font-medium"
+                        >
                            Cuotas: -
                         </td>
-                        <td class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium"
+                        >
                            Dirección:
                            <label class="font-bold">{{
                               props.detalle.cliente.direccion
-                              }}</label>
+                           }}</label>
                         </td>
                      </tr>
                      <tr>
-                        <td class="w-1/2 px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 px-2 py-1 text-left text-sm font-medium"
+                        >
                            Moneda:
                            <label class="font-bold">{{
                               props.detalle.moneda
-                              }}</label>
+                           }}</label>
                         </td>
-                        <td class="w-1/2 px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 px-2 py-1 text-left text-sm font-medium"
+                        >
                            Tipo de cambio:
                            <label class="font-bold">
-                              {{ props.detalle.cambio ?? "-" }}</label>
+                              {{ props.detalle.cambio ?? "-" }}</label
+                           >
                         </td>
                      </tr>
                      <tr>
-                        <td class="w-1/2 px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 px-2 py-1 text-left text-sm font-medium"
+                        >
                            Tipo de cambio: -
                         </td>
-                        <td class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium">
+                        <td
+                           class="w-1/2 whitespace-nowrap px-2 py-1 text-left text-sm font-medium"
+                        >
                            Correo electrónico:
                            <label class="font-bold">{{
                               props.detalle?.cliente?.email
-                              }}</label>
+                           }}</label>
                         </td>
                      </tr>
                      <!--tr>
@@ -145,121 +183,147 @@
 
          <!-- Detalle -->
          <div class="md:col-span-3 p-1 shadow-md rounded">
-            <table class="min-w-full border border-gray-200">
+            <table class="w-full table-fixed border border-gray-200">
                <thead class="bg-gray-300 text-gray-800">
                   <tr>
-                     <th scope="col"
-                        class="px-6 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wider border-b">
+                     <th
+                        scope="col"
+                        class="px-2 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wide border-b"
+                     >
                         Código
                      </th>
-                     <th scope="col"
-                        class="px-6 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wider border-b">
+                     <th
+                        scope="col"
+                        class="px-2 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wide border-b"
+                     >
                         Descripción
                      </th>
-                     <th scope="col"
-                        class="px-6 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wider border-b">
+                     <th
+                        scope="col"
+                        class="px-2 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wide border-b"
+                     >
                         Cantidad
                      </th>
-                     <th scope="col"
-                        class="px-6 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wider border-b">
+                     <th
+                        scope="col"
+                        class="px-2 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wide border-b"
+                     >
                         Precio Unitario
                      </th>
-                     <th scope="col"
-                        class="px-6 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wider border-b">
+                     <th
+                        scope="col"
+                        class="px-2 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wide border-b"
+                     >
                         Total Unitario
                      </th>
-                     <th scope="col"
-                        class="px-6 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wider border-b">
+                     <th
+                        scope="col"
+                        class="px-2 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wide border-b"
+                     >
                         Descuento
                      </th>
-                     <th scope="col"
-                        class="px-6 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wider border-b">
+                     <th
+                        scope="col"
+                        class="px-2 py-1 text-left text-xs font-bold text-gray-800 uppercase tracking-wide border-b"
+                     >
                         Monto IVA
                      </th>
                   </tr>
                </thead>
                <tbody class="bg-white divide-y divide-gray-200">
                   <tr v-for="item in props.detalle?.items" :key="item._id">
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <div class="text-sm text-gray-900">
-                           {{ item.codigo }}
-                        </div>
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{ item.codigo }}
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200">
-                        <div class="text-sm text-gray-900">
-                           {{ item.descripcion }}
-                        </div>
+                     <td class="px-2 py-1 border border-gray-200 text-sm">
+                        {{ item.descripcion }}
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <div class="text-sm text-gray-900">
-                           {{ formatNumber(item.cantidad) }}
-                        </div>
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{ formatNumber(item.cantidad) }}
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <div class="text-sm text-gray-900">
-                           {{ formatPriceNumber(item?.precioUnitario) }}
-                        </div>
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{ formatPriceNumber(item?.precioUnitario) }}
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <div class="text-sm text-gray-900">
-                           {{
-                              formatPriceNumber(
-                                 item?.precioUnitario * item?.cantidad,
-                              )
-                           }}
-                        </div>
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{
+                           formatPriceNumber(
+                              item?.precioUnitario * item?.cantidad,
+                           )
+                        }}
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <div class="text-sm text-gray-900">
-                           {{
-                              formatNumber(
-                                 item.descuento ? item.descuento : "0",
-                              )
-                           }}
-                        </div>
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{
+                           formatNumber(item.descuento ? item.descuento : "0")
+                        }}
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <div class="text-sm text-gray-900">
-                           {{ calculateIVA(item) }}
-                        </div>
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{ calculateIVA(item) }}
                      </td>
                   </tr>
                </tbody>
                <tfoot class="bg-gray-300 text-gray-800">
                   <tr>
-                     <td colspan="6" class="px-6 py-1 whitespace-nowrap border border-gray-200 font-bold">
+                     <td
+                        colspan="6"
+                        class="px-2 py-1 border border-gray-200 font-bold text-sm"
+                     >
                         Subtotal:
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
                         {{
                            formatPriceNumber(detalle.total - detalle.totalIva)
                         }}
                      </td>
                   </tr>
                   <tr>
-                     <td colspan="6" class="px-6 py-1 whitespace-nowrap border border-gray-200 font-bold">
+                     <td
+                        colspan="6"
+                        class="px-2 py-1 border border-gray-200 font-bold text-sm"
+                     >
                         Total de la operación:
                      </td>
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <!--{{ formatPriceNumber(detalle.total) }}-->
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{ formatPriceNumber(detalle.total) }}
                      </td>
                   </tr>
                   <tr>
-                     <td colspan="2" class="px-6 py-1 whitespace-nowrap border border-gray-200">
-                        <label class="font-bold">IVA (5%):</label>
-                        <!-- {{ formatPriceNumber(detalle.iva5) }} -->
+                     <td
+                        colspan="2"
+                        class="px-2 py-1 border border-gray-200 text-sm font-bold"
+                     >
+                        IVA (5%):
                      </td>
-
-                     <td colspan="3" class="px-6 py-1 whitespace-nowrap border border-gray-200">
-                        <label class="font-bold">IVA (10%):</label>
-                        <!--{{ formatPriceNumber(detalle.iva10) }} -->
+                     <td
+                        colspan="3"
+                        class="px-2 py-1 border border-gray-200 text-sm font-bold"
+                     >
+                        IVA (10%):
                      </td>
-                     <td colspan="1" class="px-6 py-1 whitespace-nowrap border border-gray-200">
-                        <label class="font-bold">Total IVA:</label>
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-sm font-bold"
+                     >
+                        Total IVA:
                      </td>
-
-                     <td class="px-6 py-1 whitespace-nowrap border border-gray-200 text-right">
-                        <!--{{ formatPriceNumber(detalle.totalIva) }}-->
+                     <td
+                        class="px-2 py-1 border border-gray-200 text-right text-sm"
+                     >
+                        {{ formatPriceNumber(detalle.totalIva) }}
                      </td>
                   </tr>
                </tfoot>
